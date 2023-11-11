af3a983796c8494e527807880b18db3fea6e9a25
Aadesh Kulkarni <aadeshkulkarni@Aadeshs-MacBook-Pro.local>
2023-11-11 18:22:34 +0530

    optimization using custom hooks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { toggleMenu, toggleSearch } from "../utils/slice/appSlice";
-import { YOUTUBE_SEARCH_API, YOUTUBE_SEARCH_BY_KEYWORD_API } from "../utils/constants";
-import { cacheResults } from "../utils/slice/searchSlice";
-import { setVideos } from "../utils/slice/videoSlice";
+import useYoutubeSearch from "../hooks/useYoutubeSearch";
 
 const Header = () => {
-  const [suggestions, setSuggestions] = useState([]);
-  const [showSuggestions, setShowSuggestions] = useState(false);
-  const [search, setSearch] = useState("");
-  const cache = useSelector((store) => store.search);
+  const youtubeSearch = useYoutubeSearch()
+  const { search, suggestions, showSuggestions,setSearch, getSearchData, setShowSuggestions } = youtubeSearch
   const dispatch = useDispatch();
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
   };
-
-  const getSearchRecommendations = async () => {
-    if (cache[search]) {
-      setSuggestions(cache[search]);
-    } else {
-      const data = await fetch(YOUTUBE_SEARCH_API + search);
-      const response = await data.json();
-      setSuggestions(response[1]);
-      if (search !== "") {
-        dispatch(cacheResults({ [search]: response[1] }));
-      }
-    }
-  };
-
-  const getSearchData = async (query) => {
-    if (search !== "") {
-      const data = await fetch(YOUTUBE_SEARCH_BY_KEYWORD_API.replace("[QUERY]", query));
-      const response = await data.json();
-      dispatch(setVideos(response?.items));
-    }
-  };
-
-  useEffect(() => {
-    const timer = setTimeout(() => getSearchRecommendations(), 500);
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [search]);
 
   const onClickHandler = (suggestion) => {
     getSearchData(suggestion);
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,22 +1,61 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleSearch } from "../utils/slice/appSlice";
+import useYoutubeSearch from "../hooks/useYoutubeSearch";
 
 const Searchbar = () => {
-const dispatch = useDispatch()
-const isSearchOpen = useSelector(store=>store.app.isSearchOpen)
-  return isSearchOpen && (<div className="md:hidden bg-black bg-opacity-40 absolute z-50 top-0 left-0 w-screen h-screen">
-      <div className="bg-gray-50 grid grid-cols-12 p-2">
-        <div onClick={()=>dispatch(toggleSearch())} className="col-span-1 font-2xl flex justify-center items-center">←</div>
-        <div className="col-span-10 relative">
-          <input type="text" className="w-full px-3 py-1 bg-gray-200 rounded-full" />
-          <img src="./search.svg" className="absolute right-2 top-1" alt="" />
+  const youtubeSearch = useYoutubeSearch();
+  const { search, suggestions, showSuggestions, setSearch, getSearchData, setShowSuggestions } = youtubeSearch;
+  const dispatch = useDispatch();
+  const isSearchOpen = useSelector((store) => store.app.isSearchOpen);
+  const onClickHandler = (suggestion) => {
+    getSearchData(suggestion);
+    setSearch(suggestion);
+    setShowSuggestions(false);
+    dispatch(toggleSearch())
+  };
+  return (
+    isSearchOpen && (
+      <div className="md:hidden bg-black bg-opacity-40 absolute z-50 top-0 left-0 w-screen h-screen overflow-hidden">
+        <div className="bg-gray-50 grid grid-cols-12 p-2">
+          <div onClick={() => dispatch(toggleSearch())} className="col-span-1 font-2xl flex justify-center items-center">
+            ←
+          </div>
+          <form
+            className="col-span-10 relative"
+            onSubmit={(e) => {
+              e.preventDefault();
+              getSearchData(search);
+            }}
+          >
+            <input 
+            type="text" 
+            value={search} 
+            onChange={(e) => setSearch(e.target.value)} 
+            onFocus={() => setShowSuggestions(true)}
+            className="w-full px-3 py-1 bg-gray-200 rounded-full" />
+            <img src="./search.svg" className="absolute right-2 top-1" alt="" />
+          </form>
+          <div className="col-span-1 font-2xl flex justify-center items-center">🎙️</div>
         </div>
-        <div className="col-span-1 font-2xl flex justify-center items-center">
-            🎙️
-        </div>
+        {showSuggestions && suggestions.length > 0 && (
+          <>
+            <div className="fixed top-0 left-0 z-10 w-screen h-screen" onClick={() => setShowSuggestions(false)}></div>
+            <div className="fixed bg-white py-2 px-4 border border-gray-300 w-screen z-50">
+              <ul className="">
+                {suggestions.map((suggestion) => (
+                  <li key={suggestion} onClick={() => onClickHandler(suggestion)} className="hover:bg-gray-100 py-2 border-b border-gray-200 cursor-pointer flex">
+                    <img className="mx-1" src="./search.svg" alt="search" />
+                    {suggestion}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </>
+        )}
       </div>
-    </div>)
-}
+    )
+  );
+};
 
 export default Searchbar;
